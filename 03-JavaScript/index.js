3dc0264f6653590eff7f96b21f853b5f2a8e7174
Anish Devkota <devkotaanish071@gmail.com>
2026-07-03 16:34:31 +0530

    Added Web technology Work

---
+++ 03-JavaScript/index.js
@@ -0,0 +1,6 @@
+console.log("Hello World!");
+window.alert("Hello King is Typing Something");
+
+
+document.getElementById("B1").textContent = "Anish";
+document.getElementById("P1").textContent = "Anish is King";
