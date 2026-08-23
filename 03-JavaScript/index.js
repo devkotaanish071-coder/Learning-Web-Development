d913231221901dae24175f6553de6d7bc1c7d19c
Anish Devkota <devkotaanish071@gmail.com>
2026-08-23 22:24:07 +0530

    Added and Reconstructed the Structure

--- 03-JavaScript/index.js
+++
@@ -1,7 +0,0 @@
-console.log("Hello World!");
-window.alert("Hello King is Typing Something");
-alert("Something is going to happen");
-
-
-document.getElementById("B1").textContent = "Anish";
-document.getElementById("P1").textContent = "Anish is King";
