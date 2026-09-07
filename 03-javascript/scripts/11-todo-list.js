79732f13b4fc0c4da9c9b23482563deeb010377b
Anish Devkota <devkotaanish071@gmail.com>
2026-09-07 22:26:17 +0530

    Learned difference between for and while loop

--- 03-javascript/scripts/11-todo-list.js
+++ 03-javascript/scripts/11-todo-list.js
@@ -1,4 +1,8 @@
-const todoList = [];
+const todoList = ['make dinner', 'cook food'];
+
+for(let i = 0; i < todoList.length; i++){
+    console.log(todoList[i]);
+}
 
 function addTodo(){
     const inputElement = document.querySelector('.js-name-input');
